c9a2f6da2ccdc8be42c33740bd19c5e5f2ab12c4
spff <spff@bitmessage.ch>
2018-03-17 00:29:41 +0800

    done

--- index.js
+++ index.js
@@ -8,19 +8,9 @@ express()
   .use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
   .get('/', (req, res) => res.status(404).send())
   .post('', bodyParser.json(), (req, res)=> {
-    //if (!req.body) return res.sendStatus(400)
-    
-
-    console.log(req.body)
-    console.log(req.is('application/json'))
-    console.log("receive POST")
-
-    //TODO validate the input, we should verify every input to make sure nothing goes wrong when the client or the scrawler got hacked
-    
+        
     console.log(JSON.stringify(req.body))
     
-    ret = res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('okay')
-    return ret
+    return res.send('okay')
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
