26872f3ba55e9421bd6c02a758a3e25d69c96ff9
spff <spff@bitmessage.ch>
2018-03-16 23:23:37 +0800

    echo request

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.status(404).send())
+  .post('/', (req, res) => res.send(req))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
